d3859fb403dd89cb1a2cc24025bcdb137bddbfcf
Ateto1202 <tonysu1204@gmail.com>
2024-02-02 23:24:59 +0800

    Attemp adding item list

--- Subproject/CustomSildePicker.swiftpm/SelectionView.swift
+++ Subproject/CustomSildePicker.swiftpm/SelectionView.swift
@@ -3,30 +3,89 @@ import SwiftUI
 struct SelectionView: View {
     
     @State var offset: CGFloat = 0
+    @State var selectTitle: String = ""
     
     var body: some View {
         GeometryReader { geometry in 
-            VStack(spacing: 0) {
-                let pickerCount = 10
-                let itemWidth: CGFloat = geometry.size.width * 0.45
-                let itemHeight: CGFloat = 60
-                let offsetAngle = sin(75.0 * Double.pi / 180)
-                packButton(geometry: geometry)
-                    .onTapGesture {
+            ZStack(alignment: .top) {
+                VStack(spacing: 0) {
+                    let pickerCount = 10
+                    let itemWidth: CGFloat = geometry.size.width * 0.45
+                    let itemHeight: CGFloat = 60
+                    let offsetAngle = sin(75.0 * Double.pi / 180)
+                    packButton(geometry: geometry)
+                        .onTapGesture {
+                            
+                        }
+                    
+                    ZStack {
+                        SlidedPicker(pickerCount: pickerCount, itemWidth: itemWidth * offsetAngle, itemHeight: itemHeight, offset: $offset, content: {
+                            ForEach(0..<10) { i in 
+                                Text("abc")
+                                    .font(.system(size: 24))
+                                    .bold()
+                                    .padding(.leading, 30)
+                                    .frame(width: itemWidth * offsetAngle, height: itemHeight, alignment: .leading)
+                                    .rotationEffect(.degrees(-15))
+                            }
+                        })
+                        .background(
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .opacity(0.1)
+                                .frame(height: geometry.size.height * 1.4)
+                        )
+                        .frame(width: itemWidth * offsetAngle)
+                        .rotationEffect(.degrees(15))
                         
+                        ZStack {
+                            ZStack(alignment: .trailing) {
+                                Diamond()
+                                    .foregroundColor(.black)
+                                    .frame(width: itemWidth)
+                                Diamond()
+                                    .foregroundColor(.white)
+                                    .frame(width: 10)
+                            }
+                            VStack {
+                                HStack(spacing: 10) {
+                                    Image(systemName: "star")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 15)
+                                    Text("Title")
+                                        .font(.system(size: 24))
+                                        .bold()
+                                }
+                                .frame(width: itemWidth, alignment: .leading)
+                                .offset(x: -25)
+                                Text("Subtitle")
+                                    .font(.system(size: 14))
+                                    .frame(width: itemWidth, alignment: .leading)
+                            }
+                            .padding(.leading, 100)
+                        }
+                        .frame(width: itemWidth, height: itemHeight * 1.2)
+                        .offset(x: -10, y: 1.6)
+                        .onChange(of: offset) { _ in 
+                            
+                        }
+                        .onChange(of: selectTitle) { _ in 
+                            
+                        }
+                        .onTapGesture(count: 2, perform: {
+                            
+                        })
                     }
-                
-                ZStack {
-                    SlidedPicker(pickerCount: pickerCount, itemWidth: itemWidth * offsetAngle, itemHeight: itemHeight, offset: $offset, content: {
-                        Text("abc")
-                            .font(.system(size: 24))
-                            .bold()
-                            .padding(.leading, 30)
-                            .frame(width: itemWidth * offsetAngle, height: itemHeight, alignment: .leading)
-                            .rotationEffect(.degrees(-15))
-                    })
                 }
+                .offset(x: -62, y: -60)
             }
+            .frame(width: geometry.size.width * 0.45, height: geometry.size.height)
+            
+            packButton(geometry: geometry)
+                .onTapGesture {
+                    
+                }
         }
     }
     
