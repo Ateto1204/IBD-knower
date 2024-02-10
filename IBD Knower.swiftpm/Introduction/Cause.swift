c4c52488ee4d0cfc747dad02566639577e9673dd
Ateto1202 <tonysu1204@gmail.com>
2024-02-10 17:29:23 +0800

    Combine resource into IBD Knower

---
+++ IBD Knower.swiftpm/Introduction/Cause.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+class Cause: IntroItem {
+    
+    init() {
+        super.init(title: "Cause", 
+                   subtitle: "Why you get IBD", 
+                   abstract: ["Immune system malfunction", 
+                              "Heredity"], 
+                   detail: [])
+    }
+}
