381e003033b77a9916a18decd62c29efa0f4f1f8
Ateto1202 <tonysu1204@gmail.com>
2024-02-14 21:23:45 +0800

    Beauty APP interface

--- IBD Knower.swiftpm/Main/HomeView.swift
+++ IBD Knower.swiftpm/Main/HomeView.swift
@@ -5,67 +5,76 @@ struct HomeView: View {
     
     var body: some View {
         GeometryReader { geometry in 
-            VStack {
-                Spacer()
-                    .frame(height: geometry.size.height * 0.21)
-                // Image(systemName: "hand.draw.fill")
-                Image(systemName: "house.fill")
-                Text("IBD KNOWER")
-                    .foregroundColor(.white)
-                    .font(.system(size: 41))
-                    .bold()
-                Spacer()
-                    .frame(height: geometry.size.height * 0.13)
-                HStack {
+            ZStack {
+                Color.gray
+                    .blur(radius: 12)
+                
+                VStack {
                     Spacer()
-                    Button {
-                        selectStatus.setStatus(newStatus: .INTRO)
-                    } label: {
-                        VStack(spacing: 16) {
-                            ZStack {
-                                Rectangle()
-                                    .frame(width: geometry.size.height * 0.28, height: geometry.size.height * 0.28)
-                                    .cornerRadius(15)
-                                Image(systemName: "books.vertical")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: geometry.size.height * 0.14)
+                        .frame(height: geometry.size.height * 0.21)
+                    // Image(systemName: "hand.draw.fill")
+                    Image(systemName: "house.fill")
+                    Text("IBD KNOWER")
+                        .foregroundColor(.white)
+                        .font(.system(size: 41))
+                        .bold()
+                    Spacer()
+                        .frame(height: geometry.size.height * 0.13)
+                    HStack {
+                        Spacer()
+                        Button {
+                            selectStatus.setStatus(newStatus: .INTRO)
+                        } label: {
+                            VStack(spacing: 16) {
+                                ZStack {
+                                    Rectangle()
+                                        .frame(width: geometry.size.height * 0.28, height: geometry.size.height * 0.28)
+                                        .cornerRadius(15)
+                                    Image(systemName: "books.vertical")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: geometry.size.height * 0.14)
+                                        .foregroundColor(.white)
+                                }
+                                Text("INTRODUCTION")
                                     .foregroundColor(.white)
+                                    .font(.system(size: 24))
+                                    .bold()
                             }
-                            Text("INTRODUCTION")
-                                .foregroundColor(.white)
-                                .font(.system(size: 24))
-                                .bold()
                         }
-                    }
-                    .buttonStyle(PushDownButtonStyle())
-                    
-                    Spacer()
-                        .frame(width: geometry.size.width * 0.1)
-                    
-                    Button {
+                        .shadow(radius: 7.2)
+                        .buttonStyle(PushDownButtonStyle())
                         
-                    } label: {
-                        VStack(spacing: 16) {
-                            ZStack {
-                                Rectangle()
-                                    .frame(width: geometry.size.height * 0.28, height: geometry.size.height * 0.28)
-                                    .cornerRadius(15)
-                                Image(systemName: "figure.cooldown")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: geometry.size.width * 0.08)
+                        Spacer()
+                            .frame(width: geometry.size.width * 0.1)
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack(spacing: 16) {
+                                ZStack {
+                                    Rectangle()
+                                        .frame(width: geometry.size.height * 0.28, height: geometry.size.height * 0.28)
+                                        .cornerRadius(15)
+                                    Image(systemName: "figure.cooldown")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: geometry.size.width * 0.08)
+                                        .foregroundColor(.white)
+                                }
+                                Text("WARM UP")
                                     .foregroundColor(.white)
+                                    .font(.system(size: 24))
+                                    .bold()
                             }
-                            Text("WARM UP")
-                                .foregroundColor(.white)
-                                .font(.system(size: 24))
-                                .bold()
                         }
+                        .shadow(radius: 7.2)
+                        .buttonStyle(PushDownButtonStyle())
+                        
+                        Spacer()
                     }
                     Spacer()
                 }
-                Spacer()
             }
         }
     }
