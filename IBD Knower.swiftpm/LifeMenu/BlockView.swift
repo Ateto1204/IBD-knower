60320f0e7d0676018c7e082b35bb00e03f96fec2
Ateto1202 <tonysu1204@gmail.com>
2024-02-23 15:56:04 +0800

    Add life menu to IBD Knower

--- IBD Knower.swiftpm/LifeMenu/BlockView.swift
+++ IBD Knower.swiftpm/LifeMenu/BlockView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct BlockView: View {
+    @EnvironmentObject var selectStatus: SelectStatus
+    
     @Binding var hasSelect: Bool
     @State private var isSelect: Bool = false
     
@@ -38,24 +40,33 @@ struct BlockView: View {
                             RoundedRectangle(cornerRadius: 25)
                                 .foregroundColor(theme)
                                 .padding()
-                            Text(selectedTitle)
-                                .foregroundColor(.white)
-                                .multilineTextAlignment(.leading)
-                                .padding()
+                                .shadow(radius: 13)
+                            HStack {
+                                Text(selectedTitle)
+                                    .foregroundColor(.white)
+                                    .multilineTextAlignment(.leading)
+                                    .padding()
+                                Spacer()
+                            }
+                            .padding()
                         }
                     }
                 }
                 .sheet(isPresented: $isSelect) {
-                    BlockItemView(selectedTitle: $selectedTitle, 
-                                  selectedPhysical: $selectedPhysical, 
-                                  selectedMental: $selectedMental,
-                                  hasSelected: $hasSelect, 
-                                  isSelected: $isSelect, 
-                                  title: title, 
-                                  items: items, 
-                                  theme: theme, 
-                                  physical: physical, 
-                                  mental: mental)
+                    ZStack {
+                        Color(selectStatus.bgColor)
+                        
+                        BlockItemView(selectedTitle: $selectedTitle, 
+                                      selectedPhysical: $selectedPhysical, 
+                                      selectedMental: $selectedMental,
+                                      hasSelected: $hasSelect, 
+                                      isSelected: $isSelect, 
+                                      title: title, 
+                                      items: items, 
+                                      theme: theme, 
+                                      physical: physical, 
+                                      mental: mental)
+                    }
                 }
             }
             .frame(height: 200)
